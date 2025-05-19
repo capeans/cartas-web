e6f9a532065b7dfee004d34399c610da3c765c74
capeans <42524823+capeans@users.noreply.github.com>
2025-05-19 21:23:26 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -111,28 +111,3 @@ function abrirImagenGrande(src) {
   document.body.appendChild(overlay);
 }
 
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".carousel-container").forEach(container => {
-    const track = container.querySelector(".carousel-track");
-    const left = container.querySelector(".carousel-btn.left");
-    const right = container.querySelector(".carousel-btn.right");
-
-    left?.addEventListener("click", () => {
-      if (track.scrollLeft <= 0) {
-        track.scrollLeft = track.scrollWidth;
-      } else {
-        track.scrollBy({ left: -300, behavior: "smooth" });
-      }
-    });
-
-    right?.addEventListener("click", () => {
-      if (track.scrollLeft + track.clientWidth >= track.scrollWidth - 10) {
-        track.scrollLeft = 0;
-      } else {
-        track.scrollBy({ left: 300, behavior: "smooth" });
-      }
-    });
-  });
-});
